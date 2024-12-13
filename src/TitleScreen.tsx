545b339d5faa9299504c7e85f31afc27c970eeeb
SAndreeaM <s.andreea001@yahoo.com>
2024-12-14 01:43:30 +0200

    refactor(ui): update logo and button styles
    - Removed background for the logo container.
    - Separated the logo into two stacked <img> components for improved styling.
    - Added transparent white overlay effect to buttons on hover.
    - Prepared to apply stacked image logic to buttons (in progress).

--- src/TitleScreen.tsx
+++ src/TitleScreen.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
 import starguess from './assets/starguess.svg';
+import starguess_bg from './assets/logo-bg.svg';
 
 const TitleScreen: React.FC = () => {
 	const buttons: string[] = ['Play', 'Settings', 'About'];
 	const buttonElements = buttons.map((buttonText, index) => (
-		<button key={index} className='title-screen-btn'>
+		<button key={index} className='title-screen-btn flexbox'>
 			{buttonText}
 		</button>
 	))
 
 	return (
 		<div className='title-screen flexbox'>
-			<div className='starguess-logo'>
-				<img src={starguess} alt="StarGuess Logo" />
+			<div className='starguess-logo-container flexbox'>
+				<img className='starguess-logo' src={starguess} alt="StarGuess Logo" />
+				<img className='starguess-logo-bg' src={starguess_bg} alt="StarGuess Logo" />
 			</div>
 			<div className='btn-container flexbox'>
 				{buttonElements}
